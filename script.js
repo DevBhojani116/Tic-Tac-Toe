41a253c0974baa68ad6cdf5f486ac33c8b43517b
DevBhojani116 <devbhojani116@gmail.com>
2023-12-19 02:30:22 +0530

    added function for better understandability

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-// document.getElementById("c1").disabled = false;
-// document.getElementById("c2").disabled = false;
-// document.getElementById("c3").disabled = false;
-// document.getElementById("c5").disabled = false;
-// document.getElementById("c4").disabled = false;
-// document.getElementById("c6").disabled = false;
-// document.getElementById("c7").disabled = false;
-// dcument.getElementById("c8").disabled = false;
-// document.getElementById("c9").disabled = false;
-
 const arr = [false,false,false,false,false,false,false,false,false];
 
 let count = 0;
 function clicked()
 {
+    let clickedElement = event.target.id;
+
+    if(isOccupied(clickedElement) == true)
+    {
+        console.log("Cell already used by another player! Try again");
+        exit();
+    }
+
     let v = "";
     if(count%2 == 0)
         v = "X";
     else
         v = "O";
-    count++;
-    let clickedElement = event.target.id;
-    if(arr[parseInt(clickedElement)-1] == true)
-    {
-        console.log("Cell already used by another player! Try again");
-        count -= 1;
-        exit();
-    }
+
     arr[parseInt(clickedElement)-1] = true;
-    console.log(clickedElement)
+    console.log(clickedElement);
+    
     document.getElementById(clickedElement).innerHTML = v;
+    count++;
     console.log(arr);
+}
+
+function isOccupied(cell)
+{
+    if(arr[cell-1] == false)
+        return false; //the cell is empty
+    else
+        return true; //the cell contains either X or O
 }
 
 
