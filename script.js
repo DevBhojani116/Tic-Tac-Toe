f3472fb8739492c880268469fa29b0fa9b5a385e
DevBhojani116 <devbhojani116@gmail.com>
2023-12-22 22:32:09 +0530

    added strikethrough lines

--- script.js
+++ script.js
@@ -76,9 +76,66 @@ function win(arr)
 {
     for(let i = 0; i<8; i++)
     {
+        let check = 0;
         if(arr[winningPossibilities[i][0]] != false)
+        {
             if(arr[winningPossibilities[i][0]] == arr[winningPossibilities[i][1]] && arr[winningPossibilities[i][1]] == arr[winningPossibilities[i][2]] && arr[winningPossibilities[i][0]] == arr[winningPossibilities[i][2]])
+            {
+                check = 1;
+            }
+            if(check === 1)
+            {
+                lines = document.getElementsByClassName("lines");
+                if(i == 2) //slantLineLR needs to be displayed
+                {
+                    lines[0].style.display = "inline";
+                }
+                else if(i == 5) //slantLineRL needs to be displayed
+                {
+                    lines[1].style.display = "inline";
+                }
+                else if(i == 6) //horizontalLine needs to be displayed in the centre column
+                {
+                    lines[2].style.display = "inline";
+                    lines[2].style.position = "relative";
+                    lines[2].style.top = "134px";
+                }
+                else if(i == 0) //horizontalLine needs to be displayed in the first column(topmost column)
+                {
+                    lines[2].style.display = "inline";
+                    lines[2].style.position = "relative";
+                    lines[2].style.top = "45px";
+                }
+                else if(i == 7) //horizontalLine needs to be displayed in the last column(bottommost column)
+                {
+                    lines[2].style.display = "inline";
+                    lines[2].style.position = "relative";
+                    lines[2].style.top = "223px";
+                }
+                else if(i == 3) //verticalLine needs to be displayed in the centre row
+                {
+                    lines[3].style.display = "inline";
+                    lines[3].style.position = "relative";
+                    lines[3].style.top = "135px";
+                    lines[3].style.left = "0px";
+                }
+                else if(i == 1) //verticalLine needs to be displayed in the first row(leftmost row)
+                {
+                    lines[3].style.display = "inline";
+                    lines[3].style.position = "relative";
+                    lines[3].style.top = "135px";
+                    lines[3].style.left = "-88px"
+                }
+                else if(i == 4) //verticalLine needs to be displayed in the last row(rightmost row)
+                {
+                    lines[3].style.display = "inline";
+                    lines[3].style.position = "relative";
+                    lines[3].style.top = "135px";
+                    lines[3].style.left = "88px"
+                }
                 return arr[winningPossibilities[i][0]];
+            }
+        }
     }
     return "none";
 }
@@ -90,6 +147,10 @@ function resetGame()
     {
         document.getElementById(i).innerHTML = "";
     } 
+    for(let i = 0; i<=3; i++)
+    {
+        document.getElementsByClassName("lines")[i].style.display = "none";
+    }
     title.innerHTML = "Start the game";
     document.getElementById("winner").innerHTML = "";
     count = 0;
